fa9c5c4606be30dc872e65c6421143e3bacac1e0
darmstrong <armstrong2579@gmail.com>
2022-08-19 12:18:48 -0700

    add webpack info, need to fix manifest pack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,8 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const { LibManifestPlugin } = require('webpack');
 
 module.exports = () => {
   return {
@@ -11,17 +15,59 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        filename: 'manifest.json',
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        description: 'My awesome Progressive Web App!',
+        background_color: '#ffffff',
+        start_url: ".",
+        crossorigin: null,
+        inject: true,
+        fingerprints: true,
+        ios: false,
+        publicPath: null,
+        includeDirectory: true,
+        icons: [
+          {
+            src: path.resolve('./assets/logo.png'),
+            size: '1024x1024',
+            purpose: 'maskable'
+          }
+        ]
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
     }
   };
 };
